99e88181e065944f3c854cfab1c1dc70035dc4aa
gaialmansa <videos.gaialmansa@gmail.com>
2025-04-01 20:28:37 +0200

    Cambio en local.sample.h

--- src/local.sample.h
+++ src/local.sample.h
@@ -1,10 +1,12 @@
-#define _DEBUG
-
-/* HOSPITAL */
-const char* ssid = "WPROFESIONALES";
-const char* pass = "temporal%";
-#define _URL "https://hospital.almansa.ovh/api/"
-/* CASA */
-const char* ssid = "Mascamierdas";
-const char* pass = "Gominolas69";
-#define _URL "http://192.168.1.248/mensajeria/api/"
+#define HOSPITAL 1
+#ifdef HOSPITAL
+    //HOSPITAL 
+    const char* ssid = "WPROFESIONALES";
+    const char* pass = "temporal%";
+    #define _URL "https://hospital.almansa.ovh/api/"
+#else
+    /* CASA */
+    const char* ssid = "Mascamierdas";
+    const char* pass = "Gominolas69";
+    #define _URL "http://192.168.1.248/mensajeria/api/"
+#endif
